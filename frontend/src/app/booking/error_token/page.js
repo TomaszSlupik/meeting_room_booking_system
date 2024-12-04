81ab3f10aecfcd1a7926fefd737bdd18ea5d1169
TomaszSlupik <slupiktomasz@gmail.com>
2024-12-04 16:28:54 +0100

    Dodanie sciezki do komponentu Error Token i do System

---
+++ frontend/src/app/booking/error_token/page.js
@@ -0,0 +1,9 @@
+import ErrorToken from "@/app/components/ErrorToken/ErrorToken";
+
+export default function MainPage() {
+    return (
+      <div>
+        <ErrorToken />
+      </div>
+    );
+  }
---
+++ frontend/src/app/booking/system/page.js
@@ -0,0 +1,10 @@
+import System from '../../components/System/System'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <System />
+    </div>
+  )
+}
